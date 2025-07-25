e7c9e60bdb32dcbc773d016c7fa03f03bba67452
Zakariya AlHadhrami <zakarya601@gmail.com>
2025-07-25 22:36:12 +0400

    Soluton for dateFashion problem.

---
+++ logic1/dateFashion.java
@@ -0,0 +1,13 @@
+public int dateFashion(int you, int date) {
+  
+  if(you >= 8 || date >= 8){
+    
+    if(you <= 2 || date <= 2){return 0;}
+    else{return 2;}
+    
+    
+  }
+  else if (you <= 2 || date <= 2){return 0;}
+  else{return 1;}
+  
+}
